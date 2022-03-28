39dc2d1e2452037e4274fb635a3f00b4ae34be82
Eugene Kubesh <codbasters@gmail.com>
2022-03-28 17:51:44 +0300

    update to ng12

--- src/public_api.ts
+++ src/public_api.ts
@@ -4,3 +4,7 @@
 
 // Modules
 export { FsToggleModule } from './app/fs-toggle.module';
+
+export { FsToggleContainerComponent } from './app/components/container/container.component';
+export { FsToggleComponent } from './app/components/toggle/toggle.component';
+export { FsToggleOptionComponent } from './app/components/options/option.component';
