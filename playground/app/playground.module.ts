8f3f043cbf1bbef43af2f32873ff22949ff740b3
Raymond Gigliotti <ray@firestitch.com>
2023-09-22 14:57:26 -0400

    npm cleanup

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -3,40 +3,31 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { FsFormModule } from '@firestitch/form';
 import { FsExampleModule } from '@firestitch/example';
-import { FsToggleModule } from '@firestitch/toggle';
 import { FsMessageModule } from '@firestitch/message';
+import { FsToggleModule } from '@firestitch/toggle';
 
-import { ToastrModule } from 'ngx-toastr';
-
-import { AppMaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { FirstExampleComponent } from './components/first-example/first-example.component';
 import { SecondExampleComponent } from './components/second-example/second-example.component';
+import { AppMaterialModule } from './material.module';
 
 
 @NgModule({
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     FsToggleModule,
     BrowserAnimationsModule,
     AppMaterialModule,
     FormsModule,
-    //FsFormModule,
     FsExampleModule.forRoot(),
     FsMessageModule.forRoot(),
-    ToastrModule.forRoot({ preventDuplicates: true }),
-  ],
-  entryComponents: [
   ],
   declarations: [
     AppComponent,
     FirstExampleComponent,
     SecondExampleComponent
-  ],
-  providers: [
   ],
 })
 export class PlaygroundModule {
